38fbb808a3fea6c2f6f8926fc51074f60ff5e7b7
Karan Suthar <karansthr@hotmail.com>
2017-12-02 10:40:25 +0530

    added citizen model

--- src/citizen/models.py
+++ src/citizen/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Citizen(User):
+    contact = models.CharField(max_length=11)
+    aadhaar  = models.CharField(max_length=12)
+    bhamshah = models.CharField(max_length=12)
+
+    class Meta:
+        verbose_name = 'Citizen'
+
+    def __str__(self):
+        return self.get_full_name()
