f099bc6cbee7ced9bdf95975785eaeed60af48bb
Karan Suthar <karansthr@hotmail.com>
2017-12-02 12:41:34 +0530

    added police model

--- src/police/models.py
+++ src/police/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+designation_choice = (
+    ('DGP', 'Director General of Police'),
+    ('ADGP', 'Addl. Director General of Police'),
+    ('IGP', 'Inspector General of Police'),
+    ('DIGP', 'Deputy Inspector General of Police'),
+    ('SPDCP', 'Superintendent of police Deputy Commissioner of Police(Selection Grade)'),
+    ('SPDCPJ', 'Superintendent of police Deputy Commissioner of Police(Junior Management Grade)'),
+    ('ASPADCP', 'Addl. Superintendent of police Addl.Deputy Commissioner of Police'),
+    ('ASP', 'Assistant Superintendent of Police'),
+    ('INSP', 'Inspector of Police'),
+    ('SUB_INSP', 'Sub Inspector of Police.'),
+    ('HVLDRM', 'Asst. Sub. Inspector/Havildar Major'),
+    ('HVLDR', 'Havildar.'),
+    ('LN', 'Lance Naik.'),
+    ('CONS', 'Constable.'),
+)
+
+
+class Police(User):
+    police_id = models.CharField(max_length=20)
+    designation = models.CharField(max_length=10, choices=designation_choice, null=True)
+    ward = models.ForeignKey('Ward', null=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Police'
+
+    def __str__(self):
+        return self.get_full_name()
+
+
+class Ward(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)  # eg: RJ01w1,RJ01w2,RJ15w5
+    address = models.CharField(max_length=255, blank=False)
+
+    def get_contacts(self):
+        contact_list = [i.contact for i in Ward.objects.get(id=self.id).contact_set]
+        return contact_list
+
+    def __str__(self):
+        return self.id
+
+
+class Contact(models.Model):
+    ward = models.ForeignKey('ward')
+    contact = models.CharField(max_length=255, blank=False)
+
+    def __str__(self):
+        return self.ward
