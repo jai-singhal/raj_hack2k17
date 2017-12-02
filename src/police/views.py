8f77c94eddefb729c688d33431adcc9781170ee8
Karan Suthar <karansthr@hotmail.com>
2017-12-02 12:50:40 +0530

    police auth added

--- src/police/views.py
+++ src/police/views.py
@@ -1,3 +1,26 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import UsersLoginForm
+
+
+def login_view(request):
+    if request.user.is_authenticated():
+        return redirect("/police/dashboard")
+    form = UsersLoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect("/police/dashboard")
+    return render(request, "police/login.html")
+
+def dashboard(request):
+    if not request.user.is_authenticated():
+        raise Http404
+    return render(request,'police/dashboard.html')
--- src/raj_hack/urls.py
+++ src/raj_hack/urls.py
@@ -1,11 +1,13 @@
 
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from comment.views import CreateComment
 from comment.views import HomePage
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^police/', include('police.urls')),
+    url(r'^citizen/', include('citizen.urls')),
     url(r'comment/ajax/create', CreateComment, name = "create_comment"),
     url(r'^$', HomePage, name = "home")
 ]
