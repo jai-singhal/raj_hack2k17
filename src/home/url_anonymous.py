4ea7451618c646589064938d12b2782c0248c15e
Jai Singhal <jaisinghal48@gmail.com>
2017-12-03 04:37:35 +0530

    new commited mj

--- src/home/url_anonymous.py
+++ src/home/url_anonymous.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, include
 
-from .views import anonymous_tip, anonymous_user_login, anonymous_dashboard
+from .views import anonymous_tip, anonymous_user_login, anonymous_dashboard, get_interact_anonymous
 
 urlpatterns = [
 
     url(r'^$', anonymous_user_login, name='anonymous_user_login'),
     url(r'^tip$', anonymous_tip, name='anonymous_tip'),
-    url(r'^dashboard$', anonymous_dashboard, name="anonymous_dashboard")
-
+    url(r'^dashboard$', anonymous_dashboard, name="anonymous_dashboard"),
+    url(r'^anonymous/get_interact/', get_interact_anonymous, name = "get_interact_anonymous")
 ]
