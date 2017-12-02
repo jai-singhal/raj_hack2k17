7d59336bce7c17d260927a30d6c9a915b4cd59f9
Karan Suthar <karansthr@hotmail.com>
2017-12-02 23:37:55 +0530

    added anonymous tip form

--- src/home/url_anonymous.py
+++ src/home/url_anonymous.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 
-from .views import anonymous_tip, anonymous_user_login
+from .views import anonymous_tip, anonymous_user_login, anonymous_dashboard
 
 urlpatterns = [
 
-    url(r'^', anonymous_user_login, name='anonymous_user_login'),
-    url(r'^tip', anonymous_tip, name='anonymous_tip'),
+    url(r'^$', anonymous_user_login, name='anonymous_user_login'),
+    url(r'^tip$', anonymous_tip, name='anonymous_tip'),
+    url(r'^dashboard$', anonymous_dashboard, name="anonymous_dashboard")
 
 ]
