b4dcf94e153989a9b0e82733d95ba59228e2bca0
Karan Suthar <karansthr@hotmail.com>
2017-12-08 15:05:44 +0530

    tweaked frontend

--- src/raj_hack/urls.py
+++ src/raj_hack/urls.py
@@ -5,10 +5,13 @@ from comment.views import CreateComment
 from comment.views import HomePage, CommentPage
 from home.views import criminal_directory
 from home.views import upload_evidence
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^about$', TemplateView.as_view(template_name="about.html")),
+    url(r'^contact', TemplateView.as_view(template_name="contact.html")),
     url(r'^police/', include('police.urls')),
     url(r'^anonymous/', include('home.url_anonymous')),
     url(r'^citizen/', include('citizen.urls')),
