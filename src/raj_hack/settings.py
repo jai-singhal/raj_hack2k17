343146e253fce41f3313c9ada78f10d8b57423ce
Manoj Jeswani <manoj.jeswani@mettl.com>
2017-12-12 00:21:40 +0530

    static root given

--- src/raj_hack/settings.py
+++ src/raj_hack/settings.py
@@ -131,5 +131,9 @@ STATICFILES_DIRS= [
    os.path.join(BASE_DIR, "static")
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+
+# MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn/')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn/")
