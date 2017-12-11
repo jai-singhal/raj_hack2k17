0502825948f347bb0bf935b3987d94234f0d0c4a
Manoj Jeswani <manoj.jeswani@mettl.com>
2017-12-12 01:25:49 +0530

    modified settings

--- src/raj_hack/settings.py
+++ src/raj_hack/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS= [
-   os.path.join(BASE_DIR, "static")
-]
+# STATICFILES_DIRS= [
+#    os.path.join(BASE_DIR, "static")
+# ]
 
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 
